48bcf067897980bac1af23df1fd24e732cace536
Leisy Ladino <82046545+leisy95@users.noreply.github.com>
2021-04-09 17:01:05 -0500

    nuevo cambio en el pull .php

--- pull.php
+++ pull.php
@@ -1,12 +1,12 @@
 <?php>
   $persona = [
                 "nombre"=>"Pedro";
-                "apellido=>"Sanchez",
+                "apellido"=>"Sanchez",
                 "edad"=>"50"
              ];
              
          foreach ($personas as $valor) {
-          echo "$valor <br>"
+          echo "$valor <br>";
           
         }
 ?>
