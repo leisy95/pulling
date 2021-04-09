03264761312f58fe991e79d0675cce0e2b0fcbd7
Leisy Ladino <82046545+leisy95@users.noreply.github.com>
2021-04-09 16:56:49 -0500

    actualizando el primer archivo .php

---
+++ pull.php
@@ -0,0 +1,12 @@
+<?php>
+  $persona = [
+                "nombre"=>"Pedro";
+                "apellido=>"Sanchez",
+                "edad"=>"50"
+             ];
+             
+         foreach ($personas as $valor) {
+          echo "$valor <br>"
+          
+        }
+?>
